04cea62ae27dee43c123dbff6494d9c1ab5228d4
osmannyildiz <iamosmannyildiz@gmail.com>
2024-09-08 11:20:43 +0300

    add JoinGameScreen

---
+++ frontend/src/screens/JoinGameScreen.tsx
@@ -0,0 +1,14 @@
+function JoinGameScreen() {
+  return (
+    <>
+      <button
+        type="button"
+        className="px-4 py-4 bg-orange-700 rounded text-fancy hover:scale-150 transition-transform"
+      >
+        Join Game
+      </button>
+    </>
+  );
+}
+
+export default JoinGameScreen;
